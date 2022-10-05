ed164532eebddafa539d2a0aa288a99be24c07a5
RobbeDierickx <44427819+ElenaDierickx@users.noreply.github.com>
2022-10-05 16:58:49 +0200

    Installed tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,16 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="container">
+        <div className="h-full w-full">
             <Head>
                 <title>Elena Dierickx</title>
                 <meta name="description" content="Portfolio of Elena Dierickx" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className=""></main>
+            <main className="h-full w-full">
+                <div className="bg-pinkblue bg-cover bg-center h-full w-full"></div>
+            </main>
 
             <footer className=""></footer>
         </div>
