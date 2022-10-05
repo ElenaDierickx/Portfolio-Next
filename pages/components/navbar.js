cf3984820e267aba81673da37158de1628c61bfc
RobbeDierickx <44427819+ElenaDierickx@users.noreply.github.com>
2022-10-05 19:39:53 +0200

    Home page

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -3,9 +3,9 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
     return (
-        <div className="w-full h-16 flex flex-row py-4 md:px-10 sm:px-5 px-3 shadow-xl items-center justify-between">
-            <h1 className="font-inter font-bold text-2xl text-center">Elena Dierickx</h1>
-            <div className="items-center sm:flex flex-row justify-between xl:w-2/5 md:w-3/5 sm:w-4/6 hidden text-xl">
+        <div className="w-full h-16 flex flex-row py-4 md:px-10 sm:px-5 px-3 shadow-xl items-center justify-between absolute top-0">
+            <h1 className="font-bold text-2xl text-center">Elena Dierickx</h1>
+            <div className="items-center sm:flex flex-row justify-between xl:w-2/5 md:w-3/5 sm:w-4/6 hidden text-xl font-medium">
                 <a>What I do</a>
                 <a>About me</a>
                 <a>My work</a>
